92dc8a20e459dfeb733aa291a9c252ce9b8578b6
Shardul Vikram Singh <shardulvikrm@gmail.com>
2026-07-21 19:03:15 +0530

    Bump to 0.8.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.xnotes"
         minSdk = 26
         targetSdk = 36
-        versionCode = 42
-        versionName = "0.8.5"
+        versionCode = 43
+        versionName = "0.8.6"
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
         }
