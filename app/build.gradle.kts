7a05f6dd104b5e039be444e3ef626d4626d3fdf6
Shardul Vikram Singh <shardulvikrm@gmail.com>
2026-08-19 17:41:10 +0530

    release: v0.8.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.xnotes"
         minSdk = 26
         targetSdk = 36
-        versionCode = 49
-        versionName = "0.8.12"
+        versionCode = 50
+        versionName = "0.8.13"
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
         }
