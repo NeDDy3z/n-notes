7e67db8cf2de6c7872eb4c5451364ebdd18b1e2b
Erik Vaněk <erikvanek0@gmail.com>
2026-09-07 11:14:54 +0200

    Bumped to 0.8.16-0.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,9 +25,9 @@ android {
         applicationId = "com.xnotes"
         minSdk = 26
         targetSdk = 36
-        versionCode = 65
+        versionCode = 66
         // <upstream xnotes version>-<n-notes fork version>
-        versionName = "0.8.15-0.13"
+        versionName = "0.8.16-0.13"
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
         }
